777601cb92b6d5e6e461937e2a027f8c0cd0cf9d
JonasVerbickas <jonasverbic@gmail.com>
2022-11-08 01:40:42 +0000

    Consistent type_as

--- OriginalCycleGAN/Generators.py
+++ OriginalCycleGAN/Generators.py
@@ -23,20 +23,18 @@ class Generator(nn.Module):
         resnet50 = torchvision.models.resnet50(pretrained=True)
         #for param in resnet50.parameters():
         #    param.requires_grad = False
-        self.res_blocks = nn.Sequential(*list(resnet50.children())[:6])
+        res_blocks = nn.Sequential(*list(resnet50.children())[:6])
         #self.res_blocks.requires_grad = False
-        self.upsample_blocks = nn.Sequential(
+        upsample_blocks = nn.Sequential(
             Conv2dTransposeBlock(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
             Conv2dTransposeBlock(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
             Conv2dTransposeBlock(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1))
-        self.last = nn.Conv2d(64, 3, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
-        self.sigmoid = nn.Sigmoid()
+        last = nn.Conv2d(64, 3, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
+        sigmoid = nn.Sigmoid()
+        self.model = nn.Sequential(*[res_blocks, upsample_blocks, last, sigmoid])
 
     def forward(self, x):
-        x = self.res_blocks(x)
-        x = self.upsample_blocks(x)
-        x = self.last(x)
-        x = self.sigmoid(x)
+        x = self.model(x)
         return x
 
 
